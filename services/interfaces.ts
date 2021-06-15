9bb3b1f1c688a7a4aa554d49dc5191548948ff5d
Selrahc <pinedaclp@gmail.com>
2021-06-16 01:37:55 +0800

    cafe send message

--- services/interfaces.ts
+++ services/interfaces.ts
@@ -468,10 +468,12 @@ export class CafeModel extends CategoryModel {
   // cafe
   app_name = "";
   app_background_color = "";
+  tokenCount = 0;
 
   fromJson(map: ResponseData): CafeModel {
     this.app_name = map.app_name;
     this.app_background_color = map.app_background_color ?? "";
+    this.tokenCount = map.tokenCount ?? 0;
     super.fromJson(map);
     return this;
   }
