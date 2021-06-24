a714cdbc07582eecee251dabca15590db98d887a
Selrahc <pinedaclp@gmail.com>
2021-06-24 17:34:04 +0800

    weather widget

--- components/weather/OpenWeatherMap.vue
+++ components/weather/OpenWeatherMap.vue
@@ -1,38 +1,81 @@
 <template>
-  <section>
-    {{ weather }}
-    <!-- <h5 class="mb-0">
-      {{ weather.city }}, {{ weather.twoDigitCode }}
-      {{ Math.round(weather.current.main.temp) }}℃
-      <img
-        :src="
-          'https://openweathermap.org/img/wn/' +
-          weather.current.weather[0].icon +
-          '.png'
-        "
-      />
-    </h5>
-    <div>Feels like {{ weather.current.main.feels_like }} ℃.</div>
-    <div class="text-capitalize">
-      {{ weather.current.weather[0].description }}
+  <section class="weather" v-if="weather.name">
+    <div class="fs-xl text-center">
+      <span> {{ weather.name }},</span>
+      <span>{{ weather.sys.country }}</span>
     </div>
-    <div>Humidity: {{ weather.current.main.humidity }}%</div>
-    <div>
-      Min {{ Math.round($current.main.temp_min) }} ℃. - Max
-      {{ Math.round($current.main.temp_max) }} ℃.
-    </div> -->
+    <div class="d-flex">
+      <div class="d-flex justify-content-between">
+        <img
+          :src="
+            'https://openweathermap.org/img/wn/' +
+            weather.weather[0].icon +
+            '@2x.png'
+          "
+        />
+
+        <div class="fs-xxl mt-2">
+          {{ Math.round(weather.main.temp)
+          }}<span class="fs-xxs valign-super">℃</span>
+        </div>
+        <div class="mt-4 pl-3">
+          <div>
+            L {{ Math.round(weather.main.temp_min)
+            }}<span class="fs-xs valign-super">℃</span>
+          </div>
+          <div>
+            H {{ Math.round(weather.main.temp_max)
+            }}<span class="fs-xs valign-super">℃</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="d-flex flex-wrap justify-content-between">
+      <div class="text-capitalize">
+        {{ weather.weather[0].description }}
+      </div>
+      <div>{{ "humidity" | t }} {{ weather.main.humidity }}%</div>
+
+      <div>{{ "wind" | t }} {{ weather.wind.speed }}km/h</div>
+      <div>{{ "sunrise" | t }} {{ sunrise }}</div>
+      <div>{{ "sunset" | t }} {{ sunset }}</div>
+    </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.fs-xxs {
+  font-size: 0.5em;
+}
+.valign-super {
+  vertical-align: super;
+}
+</style>
 
 <script lang="ts">
 import { ApiService } from "@/x-vue/services/api.service";
 import { ResponseData } from "@/x-vue/services/interfaces";
+import dayjs from "dayjs";
 import Vue from "vue";
+
+import Component from "vue-class-component";
+
+@Component({})
 export default class OpenWeatherMap extends Vue {
   weather: ResponseData = {};
 
+  get sunrise(): string {
+    if (!this.weather.sys) return "";
+    return dayjs(this.weather.sys.sunrise * 1000).format("h:ma");
+  }
+
+  get sunset(): string {
+    if (!this.weather.sys) return "";
+    return dayjs(this.weather.sys.sunset * 1000).format("h:ma");
+  }
+
   async mounted(): Promise<void> {
-    console.log("OpenWeatherMap##############");
     try {
       this.weather = await ApiService.instance.openWeatherMap();
     } catch (e) {
