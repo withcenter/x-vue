84c53295a2a4904615080c479c85044fbd7d41fd
JaeHo Song <thruthesky@gmail.com>
2021-06-30 18:52:23 +0900

    update on service

--- services/x-vue.service.ts
+++ services/x-vue.service.ts
@@ -1,37 +1,30 @@
-// import router from "@/router";
+import router from "@/router";
 
-// export default class Service {
-//   // private constructor() {
-//   //   console.log("Service::construcotr");
-//   // }
-//   private static _instance: Service;
-//   public static get instance(): Service {
-//     if (!Service._instance) {
-//       Service._instance = new Service();
-//     }
-//     return Service._instance;
-//   }
+export default class Service {
+  // private constructor() {
+  //   console.log("Service::construcotr");
+  // }
+  private static _instance: Service;
+  public static get instance(): Service {
+    if (!Service._instance) {
+      Service._instance = new Service();
+    }
+    return Service._instance;
+  }
 
-//   // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-//   // vm: any;
-
-//   // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-//   // init(options: { vm: any }): void {
-//   //   this.vm = options.vm;
-//   //   // console.log("XHelper::init", this.vm);
-//   // }
-
-//   alert(title: string, message: string) {
-//     alert(title + "\n" + message);
-//   }
-//   error(e: string) {
-//     this.alert("Error", e);
-//   }
-//   confirm(message: string): boolean {
-//     return this.confirm(message);
-//   }
-//   open(path: string) {
-//     router.push(path);
-//     // this.vm._router.push(path);
-//   }
-// }
+  alert(title: string, message: string) {
+    alert(title + "\n" + message);
+  }
+  error(e: string) {
+    this.alert("Error", e);
+  }
+  confirm(message: string): boolean {
+    return confirm(message);
+  }
+  toast(title: string, message: string) {
+    this.alert(title, message);
+  }
+  open(path: string) {
+    router.push(path);
+  }
+}
