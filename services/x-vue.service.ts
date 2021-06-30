d6491fb240d337c4d5b56f12a3c492579f560d95
JaeHo Song <thruthesky@gmail.com>
2021-06-30 18:40:49 +0900

    update

--- services/x-vue.service.ts
+++ services/x-vue.service.ts
@@ -1,34 +1,37 @@
-export default class Service {
-  // private constructor() {
-  //   console.log("Service::construcotr");
-  // }
-  private static _instance: Service;
-  public static get instance(): Service {
-    if (!Service._instance) {
-      Service._instance = new Service();
-    }
-    return Service._instance;
-  }
+// import router from "@/router";
 
-  // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  // vm: any;
+// export default class Service {
+//   // private constructor() {
+//   //   console.log("Service::construcotr");
+//   // }
+//   private static _instance: Service;
+//   public static get instance(): Service {
+//     if (!Service._instance) {
+//       Service._instance = new Service();
+//     }
+//     return Service._instance;
+//   }
 
-  // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  // init(options: { vm: any }): void {
-  //   this.vm = options.vm;
-  //   // console.log("XHelper::init", this.vm);
-  // }
+//   // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+//   // vm: any;
 
-  alert(title: string, message: string) {
-    alert(title + "\n" + message);
-  }
-  error(e: string) {
-    this.alert("Error", e);
-  }
-  confirm(message: string): boolean {
-    return this.confirm(message);
-  }
-  open(path: string) {
-    // this.vm._router.push(path);
-  }
-}
+//   // // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+//   // init(options: { vm: any }): void {
+//   //   this.vm = options.vm;
+//   //   // console.log("XHelper::init", this.vm);
+//   // }
+
+//   alert(title: string, message: string) {
+//     alert(title + "\n" + message);
+//   }
+//   error(e: string) {
+//     this.alert("Error", e);
+//   }
+//   confirm(message: string): boolean {
+//     return this.confirm(message);
+//   }
+//   open(path: string) {
+//     router.push(path);
+//     // this.vm._router.push(path);
+//   }
+// }
