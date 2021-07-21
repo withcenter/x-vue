8347f0683d0f6647332d87638bc3ebf639699f1c
JaeHo Song <thruthesky@gmail.com>
2021-07-21 23:42:16 +0900

    fix. vue

--- components/user/Register.vue
+++ components/user/Register.vue
@@ -1,19 +1,12 @@
 <template>
-  <div data-cy="register-form">
+  <div class="register-form">
     <h1>{{ "user_register" | t }}</h1>
     <form @submit.prevent="$emit('submit', $event, form)">
       <b-form-group class="mb-2" :label="'email' | t" label-for="email">
-        <b-form-input
-          id="email"
-          v-model="form.email"
-          type="email"
-          :placeholder="'enter_email' | t"
-          required
-        ></b-form-input>
+        <b-form-input v-model="form.email" type="email" :placeholder="'enter_email' | t" required></b-form-input>
       </b-form-group>
       <b-form-group class="mb-2" :label="'password' | t" label-for="password">
         <b-form-input
-          id="password"
           v-model="form.password"
           type="password"
           :placeholder="'enter_password' | t"
@@ -22,7 +15,7 @@
       </b-form-group>
 
       <b-form-group class="mb-2" :label="'name' | t" label-for="name">
-        <b-form-input id="name" v-model="form.name" type="text" :placeholder="'enter_name' | t" required></b-form-input>
+        <b-form-input v-model="form.name" type="text" :placeholder="'enter_name' | t" required></b-form-input>
       </b-form-group>
 
       <button type="submit" class="btn btn-primary px-5">
